45e3107c99580966f178bb8a2187393b42fd4fb4
Matthieu Coulon <Matthieu Coulon>
2016-02-05 14:32:37 +0100

    maj

--- colorToolSass/javascripts/js.js
+++ colorToolSass/javascripts/js.js
@@ -8,7 +8,7 @@ $(function() {
             $('#demo').append("<a href=\"#\" class=\"copy-me\"><div class=\"col-xm-2 col-sm-2 col-md-2 col-lg-2 "+couleur+"-lighter-"+i+" text-white\" id=\""+couleur+"-lighter-"+i+"\"><h4>"+couleur+" lighter "+i+"</h4></div></a>");
         }
         $('#demo').append("<a href=\"#\" class=\"copy-me\"><div class=\"col-xm-2 col-sm-2 col-md-2 col-lg-2 "+couleur+" text-white\" id=\""+couleur+"\"><h4>"+couleur+"</h4></div></a>");
-        for ( i = 5; i> 0; i-- ){
+        for ( i = 1; i< 6; i++ ){
             $('#demo').append("<a href=\"#\" class=\"copy-me\"><div class=\"col-xm-2 col-sm-2 col-md-2 col-lg-2 "+couleur+"-darker-"+i+" text-white\" id=\""+couleur+"-darker-"+i+"\"><h4>"+couleur+" darker "+i+"</h4></div></a>");
         }
 
